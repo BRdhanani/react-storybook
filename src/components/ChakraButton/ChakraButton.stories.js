dc16cfae103ae67096179b911575290360fbd388
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-09-05 22:17:29 +0530

    addon added

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { Button } form '@chakra-ui/core'
+import { Button } from '@chakra-ui/core'
 
 export default {
 	title: 'Chakra/Button',
-	component: Button
+	component: Button,
+	argTypes: {
+		backgroundColor: {
+			control: 'color'
+		},
+		children: {
+			control: 'text'
+		}
+	}
 }
 
-export const Success = () => <Button variantColor="green">Success</Button>
-export const Danger = () => <Button variantColor="red">Danger</Button>
+const Template = args => <Button {...args} />
+export const Success = Template.bind({})
+Success.args = {
+	variantColor: 'green',
+	children: 'success'
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+	variantColor: 'red',
+	children: 'success'
+}
